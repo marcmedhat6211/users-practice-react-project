aa718e3c8f2cc63ca03c53d202736f04d0621f02
Marc Medhat <marcmedhat6211@gmail.com>
2022-03-10 21:19:16 +0200

    code refactoring

--- src/Components/NewUser/NewUserForm.js
+++ src/Components/NewUser/NewUserForm.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Input from "../UI/Input";
 import Button from "../UI/Button";
+import ErrorMessage from "../UI/ErrorMessage";
 import classes from "./NewUserForm.module.css";
+import errorMessageclasses from "../UI/ErrorMessage.module.css";
 
 const NewUserForm = (props) => {
   const [username, setUsername] = useState("");
   const [age, setAge] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const changeUsernameHandler = (enteredUsername) => {
     setUsername(enteredUsername);
@@ -15,16 +18,50 @@ const NewUserForm = (props) => {
     setAge(enteredAge);
   };
 
+  const validateForm = () => {
+    let returnedObject = {
+      valid: true,
+      message: null,
+    };
+
+    if (username.length === 0) {
+      returnedObject.valid = false;
+      returnedObject.message = "Username is required";
+    }
+
+    if (age.length === 0) {
+      returnedObject.valid = false;
+      returnedObject.message = "Age is required";
+    }
+
+    if (username.length === 0 && age.length === 0) {
+      returnedObject.valid = false;
+      returnedObject.message = "Username and age are required";
+    }
+
+    if (age < 0) {
+      returnedObject.valid = false;
+      returnedObject.message = "Can't enter a negative value for the age";
+    }
+
+    return returnedObject;
+  };
+
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    const userData = {
-      id: Math.random().toString(),
-      name: username,
-      age: age,
-    };
-    props.onSubmitUserForm(userData);
-    setUsername("");
-    setAge("");
+    const isFormValid = validateForm();
+    if (isFormValid.valid) {
+      const userData = {
+        id: Math.random().toString(),
+        name: username,
+        age: age,
+      };
+      props.onSubmitUserForm(userData);
+      setUsername("");
+      setAge("");
+    } else {
+      setIsValid(false);
+    }
   };
 
   return (
@@ -39,7 +76,8 @@ const NewUserForm = (props) => {
         type="number"
         onChangeInput={changeAgeHandler}
       />
-      <Button type="submit" buttonText="Add User" />
+      <Button type="submit">Add User</Button>
+      <ErrorMessage messageText="Error here" show={!isValid} />
     </form>
   );
 };
--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -2,8 +2,13 @@ import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button type={props.type} className={classes.button}>
-      {props.buttonText}
+    <button
+      // this syntax means if the props.type is undefined use the 'button' type
+      type={props.type || "button"}
+      className={classes.button}
+      onClick={props.onClick}
+    >
+      {props.children}
     </button>
   );
 };
